2d567fa5a7c68ca78faceb1cf2a243ec48d8abef
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2021-01-21 21:15:18 -0300

    Update results method in views.py

--- my_site/polls/views.py
+++ my_site/polls/views.py
@@ -1,6 +1,6 @@
 from django.urls import reverse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
 
 from .models import Choice, Question
 
@@ -17,7 +17,8 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse('You\'re looking at the results of question {}.'.format(question_id))
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
